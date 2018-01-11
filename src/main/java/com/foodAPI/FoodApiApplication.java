d8b3e237bc017889ae57feeeff2570ded595c7a0
ztjustin <justinzu93@gmail.com>
2018-01-11 15:32:58 -0600

    init

---
+++ src/main/java/com/foodAPI/FoodApiApplication.java
@@ -0,0 +1,12 @@
+package com.foodAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApiApplication.class, args);
+	}
+}
